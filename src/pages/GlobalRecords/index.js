eb2ade99f4725d3d6e2183b95ef989171789812f
Alessandro Eppacher <alessandroeppacher@gmail.com>
2021-02-28 12:23:37 -0500

    global records feature

--- src/pages/GlobalRecords/index.js
+++ src/pages/GlobalRecords/index.js
@@ -1,23 +1,59 @@
 // Globals
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useStore } from "react-redux";
+import { RECORD_TYPES } from "../../services/records";
+import { mockFetch } from "../../util/mockFetch";
 
 // Components
 import { Record } from "components/Record";
 
-// Misc
-import { data } from "components/Record/data";
+const FetchStatus = {
+  not_fetching: 0,
+  fetching: 1,
+  failed: 2,
+  succeeded: 3,
+};
 
 // Component
 function GlobalRecords() {
+  const [fetchState, setFetchState] = useState(FetchStatus.not_fetching);
+  const store = useStore();
+  const data = useSelector((state) => state.records.data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setFetchState(FetchStatus.fetching);
+        const payload = await mockFetch();
+        store.dispatch({ type: RECORD_TYPES.ADD_RECORDS, payload });
+        setFetchState(FetchStatus.succeeded);
+      } catch (e) {
+        setFetchState(FetchStatus.failed);
+      }
+    };
+    fetchData();
+
+    return store.dispatch({ type: RECORD_TYPES.CLEAR_RECORDS });
+  }, []);
+
+  const renderData = () => {
+    switch (fetchState) {
+      case FetchStatus.succeeded:
+        return data.map((record) => {
+          return <Record key={record.id} data={record} />;
+        });
+      case FetchStatus.failed:
+        return <p>Fetch error please refresh</p>;
+      case FetchStatus.fetching:
+        return <p>retrieving results</p>;
+    }
+  };
+
   return (
     <div className="aura-page aura-global_records">
       <h1>Top Records of 2020</h1>
 
-      <div className="aura-page-content">
-        {data.map((record) => {
-          return <Record key={record.id} data={record} />;
-        })}
-      </div>
+      <div className="aura-page-content">{renderData()}</div>
     </div>
   );
 }
--- src/services/records/index.js
+++ src/services/records/index.js
@@ -1,1 +1,28 @@
-export const records = () => null;
+const ADD_RECORDS = "ADD_RECORDS";
+const CLEAR_RECORDS = "CLEAR_RECORDS";
+
+export const RECORD_TYPES = {
+  ADD_RECORDS,
+  CLEAR_RECORDS,
+};
+
+const initialState = {
+  data: [],
+};
+
+export const records = (state = initialState, action) => {
+  switch (action.type) {
+    case RECORD_TYPES.ADD_RECORDS:
+      return {
+        ...state,
+        data: state.data.concat(action.payload),
+      };
+    case RECORD_TYPES.CLEAR_RECORDS:
+      return {
+        ...state,
+        data: [],
+      };
+    default:
+      return state;
+  }
+};
